bd1ed264616007360d6bec241c0fcf84a06ef940
John <bulk.yard@protonmail.com>
2019-11-22 23:39:36 +0100

    Solution for Exercise 1 and Exercise 2

--- FirstClasses/average/src/average.erl
+++ FirstClasses/average/src/average.erl
@@ -1,6 +1,28 @@
 -module(average).
 
--export([average/1]).
+-export([average/1, reduce/2]).
 
-average(_List) ->
-    ok.
+% That was tricky one but figured it out even before compilation
+% If the List empty then we can't divide on 0
+average(List) ->
+    case List of 
+        [] -> 0;
+        _List -> reduce(0, List)/length(List)
+    end.
+
+% Problems that I have encountered:
+% 1. Used to write variable with lowercase
+%    and since in erlang that is atom I had runtime errors.
+%    had to rename from `acc` to `Acc`
+% 2. `case` should end with `end` keyword.
+%    It's written in the docs but I guys I have skipepd it
+% 3. Have hard time to grasp the Pattern / Guard thing.
+%    Had some experience with Scala but long time ago.
+%    Will some practice
+reduce(Acc, [H|T]) ->
+    case T of
+        _List when length(T) > 0 -> 
+            reduce(Acc + H, T);
+        [] ->
+            Acc + H
+        end.
--- FirstClasses/sum_three/src/sum_three.erl
+++ FirstClasses/sum_three/src/sum_three.erl
@@ -1,6 +1,8 @@
 -module(sum_three).
 
+%add_three = fun(A,B,C) -> A+B+C end.
+
 -export([add_three/3]).
 
 add_three(A, B, C) ->
-    ok.
+    A+B+C.
