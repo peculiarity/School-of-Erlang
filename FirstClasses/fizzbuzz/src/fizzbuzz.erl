6f64fc7c880b550bec736338f4ca0e16d80f5ab9
John <bulk.yard@protonmail.com>
2019-11-23 23:41:18 +0100

    First solution to Exercise 3 (FizzBuzz)

--- FirstClasses/fizzbuzz/src/fizzbuzz.erl
+++ FirstClasses/fizzbuzz/src/fizzbuzz.erl
@@ -3,4 +3,17 @@
 -export([fizzbuzz/0]).
 
 fizzbuzz() ->
-    ok.
+    lists:map(
+        fun(Number) -> 
+           case Number of
+                _FizzBuzz when Number rem 3 == 0 andalso Number rem 5 == 0 ->
+                   'fizzbuzz';
+                _Fizz when Number rem 3 == 0 ->
+                   'fizz';
+                _Buzz when Number rem 5 == 0 ->
+                   'buzz';
+                _Else -> Number
+            end
+        end,
+        lists:seq(1,100)
+    ).
