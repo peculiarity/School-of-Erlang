a81265db7e6cdf7499da6acdebf521c78fa64e6f
John <bulk.yard@protonmail.com>
2019-11-24 21:25:06 +0100

    Solution to exercise 4

--- FirstClasses/fibonacci_sum/src/fibonacci_sum.erl
+++ FirstClasses/fibonacci_sum/src/fibonacci_sum.erl
@@ -4,7 +4,20 @@
 
 fibonacci_sum() ->
     Limit = 4 * 1000 * 1000,
-    fibonacci_sum(1, 1, 0, Limit).
+    fibonacci_sum(1, 1, 0, Limit)
+.
 
-fibonacci_sum(_Prev, _Curr, _Sum, _Limit) ->
-    1.
+% This is a pretty straight-forward solution.
+% Had Ex1. solved by using tail recursion so wasn't that hard.
+
+fibonacci_sum(Prev, Curr, Sum, Limit) ->
+    Next = Curr + Prev,
+    io:format("Next number is: ~p~n", [Next]),
+    case Curr of
+        _Even when Curr =< Limit andalso Curr rem 2 == 0 -> 
+            fibonacci_sum(Curr, Next, Sum+Curr, Limit);
+        _Odd when Curr =< Limit andalso Curr rem 2 == 1 -> 
+            fibonacci_sum(Curr, Next, Sum, Limit);
+        _OutOfBound when Next > Limit -> Sum
+    end    
+.
