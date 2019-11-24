a520deb79775cb40423ce10580058b32115c6e61
John <bulk.yard@protonmail.com>
2019-11-24 21:32:21 +0100

    Refactoring of the Exercise 4 Solution.

--- FirstClasses/fibonacci_sum/src/fibonacci_sum.erl
+++ FirstClasses/fibonacci_sum/src/fibonacci_sum.erl
@@ -9,14 +9,20 @@ fibonacci_sum() ->
 
 % This is a pretty straight-forward solution.
 % Had Ex1. solved by using tail recursion so wasn't that hard.
+% If you check first version it was kind of full of unused variables.
+% I noticed that it happens a lot and I've been using this approach in all solutions
+% then I read the docs and basically it's 
+% case Exp1 of  - and when Exp1 is evaluated it goes through pattern matching.
+% That's why I replaced the unsed variales with evaluated values + using Guard for the bound
 
 fibonacci_sum(Prev, Curr, Sum, Limit) ->
     Next = Curr + Prev,
+    InBound = Curr =< Limit,
     io:format("Next number is: ~p~n", [Next]),
-    case Curr of
-        _Even when Curr =< Limit andalso Curr rem 2 == 0 -> 
+    case Curr rem 2 of
+        0 when InBound -> 
             fibonacci_sum(Curr, Next, Sum+Curr, Limit);
-        _Odd when Curr =< Limit andalso Curr rem 2 == 1 -> 
+        1 when InBound -> 
             fibonacci_sum(Curr, Next, Sum, Limit);
         _OutOfBound when Next > Limit -> Sum
     end    
