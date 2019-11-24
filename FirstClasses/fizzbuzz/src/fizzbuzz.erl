31bfbc4f5fa4db9511140a051aeb265c469a49ff
John <bulk.yard@protonmail.com>
2019-11-24 23:13:48 +0100

    Solution 2 for Exercise 3

--- FirstClasses/fizzbuzz/src/fizzbuzz.erl
+++ FirstClasses/fizzbuzz/src/fizzbuzz.erl
@@ -1,10 +1,11 @@
 -module(fizzbuzz).
 
--export([fizzbuzz/0]).
+-export([fizzbuzz/0, fizzbuzz2/0]).
 
 % I ran the tests but basically there is this assertion over small list of 5 elements.
 % The issue is that fizzbuzz is of arity 0 and the task asked to solve fizzbuzz for the 1..100 range.
 
+% Version 1
 fizzbuzz() ->
     lists:map(
         fun(Number) -> 
@@ -20,3 +21,15 @@ fizzbuzz() ->
         end,
         lists:seq(1,100)
     ).
+
+% Version 2 ( after re-reading the documentation for Functions
+fizzbuzz2() -> 
+   lists:map(
+      fun(Number) -> fb(Number) end,
+      lists:seq(1, 100)
+   ).
+
+fb(Number) when Number rem 3 == 0 andalso Number rem 5 ==0 -> fizzbuzz;
+fb(Number) when Number rem 3 == 0 -> fizz;
+fb(Number) when Number rem 5 == 0 -> buzz;
+fb(Number) -> Number.
